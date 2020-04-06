a245f43c0f3fb9dae0ca68ba1f179a59f6346d5a
limed <limed@mozilla.com>
2020-04-06 14:23:09 -0700

    Update to test flux

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/limed/travis-test
+
+go 1.14
--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"fmt"
+	"html"
+	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", handler)
+	log.Println("Listening on localhost:8081")
 	http.ListenAndServe(":8081", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!")
+	fmt.Fprintf(w, "Hello %q", html.EscapeString(r.URL.Path))
 }
